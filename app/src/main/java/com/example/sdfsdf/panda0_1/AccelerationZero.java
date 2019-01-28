1533a96f612de86df28c40e2c7184255a7860cf2
tadeusz <tadeusz128@gmail.com>
2019-01-28 15:54:44 +0100

    first commit

--- app/src/main/java/com/example/sdfsdf/panda0_1/AccelerationZero.java
+++ app/src/main/java/com/example/sdfsdf/panda0_1/AccelerationZero.java
@@ -14,7 +14,7 @@ boolean isTwoObjects=false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_acceleration_zero);
+        setContentView(R.layout.activity_acceleration_zero);  
 
    }
 
