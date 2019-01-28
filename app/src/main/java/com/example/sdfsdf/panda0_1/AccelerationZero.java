a2e19baa48e7e6bf02c4cc087ec6211ee4cd3dc3
tadeusz <tadeusz128@gmail.com>
2019-01-28 16:40:37 +0100

    bugi

--- app/src/main/java/com/example/sdfsdf/panda0_1/AccelerationZero.java
+++ app/src/main/java/com/example/sdfsdf/panda0_1/AccelerationZero.java
@@ -2,11 +2,6 @@ package com.example.sdfsdf.panda0_1;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.RadioButton;
-import android.widget.TextView;
 
 public class AccelerationZero extends Activity {
 boolean isTwoObjects=false;
@@ -14,7 +9,7 @@ boolean isTwoObjects=false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_acceleration_zero);
+        setContentView(R.layout.kinematicDataGiven);
 
    }
 
